a481f7042f5c757cbdc66c907a8723483e9ca99a
Daliabulatifa <74101819+Daliabulatifa@users.noreply.github.com>
2021-01-26 12:07:46 +0200

    Add files via upload

---
+++ ass hoisting/Hoisting.js
@@ -0,0 +1,86 @@
+// var hello = 'world';
+// console.log(hello);                                   
+///////////////////////////////////////////////////
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+//////////////////////////////////////////////////////////
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// var brendan;
+// brendan='super cool';
+// function print(){
+//     var brendan ;
+//     brendan= 'only okay';
+//     console.log(brendan);
+
+// }
+// console.log(brendan);
+/////////////////////////////////////////////////////////
+/*4*/
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+// var food ; 
+// food = ' chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     var food;
+//     food= 'half-chicken';
+//     console.log(food);
+//     var food;
+//     food = 'gone';
+// }
+
+/////////////////////////////////////////////////////////////
+
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+///////////////////////////////////
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+//////////////////////////////////
+dojo = "san jose";
+console.log(dojo);
+learn();
+function learn() {
+    dojo = "seattle";
+    console.log(dojo);
+    var dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+
+
